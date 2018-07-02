8565528c09e5a15dd9044791b024226f299d0028
Sreeja Nair <40268484+siya-nair@users.noreply.github.com>
2018-07-02 18:04:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import LittleEnglish from './LittleEnglish';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+        <h1>Little English</h1>
+      </div>
+      <LittleEnglish/>
+      </div>
+    );
+  }
+}
+
+export default App;
